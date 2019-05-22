0418fa3aa630b0f6afeea99cc448b95a2e798110
roopkt@gmail.com <roopkt@gmail.com>
2019-05-22 19:28:01 -0400

    fixed the error on jsonwebtoken

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,9 +1,9 @@
-const jwt = required('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 
 function generateToken(user) {
   const payload = JSON.stringify(user);
-  return jwt.sign(payload, config.jwtSecret, { expireIn: '1h' });
+  return jwt.sign(payload, config.jwtSecret);
 }
 
 module.exports = { generateToken };
