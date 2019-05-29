fe4ac7aaee42e80fa4fcd98240573e47be934add
unknown <roopkt@gmail.com>
2019-05-29 19:01:55 -0400

    adding compression

--- server/config/express.js
+++ server/config/express.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const path = require('path');
-const config = require('./config');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const routes = require('../routes');
-const passport = require('../middleware/passport');
+const express = require("express");
+const path = require("path");
+const config = require("./config");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const routes = require("../routes");
+const passport = require("../middleware/passport");
+const compress = require("compression");
 
 // get app
 const app = express();
 
 // logger
-if (config.env === 'development') {
-  app.use(logger('dev'));
+if (config.env === "development") {
+  app.use(logger("dev"));
 }
 
 // get dist folder
-const distDir = path.join(__dirname, '../../dist');
+const distDir = path.join(__dirname, "../../dist");
 
 // use dist flder as hosting folder by express
 app.use(express.static(distDir));
@@ -35,10 +36,13 @@ app.use(cors());
 // authenticate
 app.use(passport.initialize());
 
+// compression
+app.use(compress());
+
 // api router localhost:4050/api
-app.use('/api/', routes);
+app.use("/api/", routes);
 
 // serve the index.html
-app.get('*', (req, res) => res.sendFile(path.join(distDir, 'index.html')));
+app.get("*", (req, res) => res.sendFile(path.join(distDir, "index.html")));
 
 module.exports = app;
