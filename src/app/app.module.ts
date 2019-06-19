10836c9534221d2ce80c885630ad00a7b5771084
Rupesh Kumar Tiwari <roopkt@gmail.com>
2019-06-19 18:19:14 -0400

    adde shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { PmMaterialModule } from "./material-module";
-import { HomeComponent } from "./home/home.component";
-import { LoginComponent } from "./login/login.component";
-import { RegisterComponent } from "./register/register.component";
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
-import { AuthHeaderInterceptorService } from "./interceptors/auth-header-interceptor.service";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PmMaterialModule } from './shared/material-module';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthHeaderInterceptorService } from './interceptors/auth-header-interceptor.service';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -24,9 +25,7 @@ import { AuthHeaderInterceptorService } from "./interceptors/auth-header-interce
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    PmMaterialModule,
-    FormsModule,
-    ReactiveFormsModule,
+    SharedModule,
     HttpClientModule
   ],
   providers: [
