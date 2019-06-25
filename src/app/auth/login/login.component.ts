33a3769fc430af87ad4a9eb63904d1f1733ff3b3
Rupesh Kumar Tiwari <roopkt@gmail.com>
2019-06-25 18:20:39 -0400

    on push strategy

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,24 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Input
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/auth/auth.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'pm-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent implements OnInit {
   email: string;
-  error: string;
+  error: BehaviorSubject<string>;
   password: string;
   constructor(private router: Router, private authService: AuthService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.error = new BehaviorSubject('');
+  }
 
   login() {
-    this.error = '';
+    this.setError('');
     this.authService
       .login(this.email, this.password)
-      .subscribe(s => this.router.navigate(['']), e => (this.error = e));
+      .subscribe(s => this.router.navigate(['']), e => this.setError(e));
+  }
+
+  private setError(msg: any) {
+    return this.error.next(msg);
   }
 }
